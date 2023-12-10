a8752311fccf9046d1ffd77a35b3a569b8e90ac2
venijamin <venijamintod@gmail.com>
2023-12-10 14:52:12 +0100

    [Add] N Tier architecture, IRepository

---
+++ 1.BulkyWeb/Bulky.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,16 @@
+using System.Linq.Expressions;
+
+namespace Bulky.DataAccess.Repository.IRepository;
+
+public interface IRepository<T> where T : class
+{
+    // T- Category
+    IEnumerable<T> GetAll();
+    T Get(Expression<Func<T, bool>> filter);
+    void Add(T entity);
+    void Update(T entity);
+    void Remove(T entity);
+    void RemoveRange(IEnumerable<T> entity);
+    void Save();
+
+}
