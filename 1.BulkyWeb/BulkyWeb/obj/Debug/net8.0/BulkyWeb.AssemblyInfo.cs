d3724bfb7f54f69bec07817bdf8a51966db7df4f
venijamin <venijamintod@gmail.com>
2023-12-10 22:05:21 +0100

    [Add] Areas and Dropdown menu

--- 1.BulkyWeb/BulkyWeb/obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
+++ 1.BulkyWeb/BulkyWeb/obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ccb86211c9a22bec1ed3b85417b61970b4b725a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8752311fccf9046d1ffd77a35b3a569b8e90ac2")]
 [assembly: System.Reflection.AssemblyProductAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
